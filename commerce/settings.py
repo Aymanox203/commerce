2157fe8080b17fe5a6fad4bf9947c4caaecb2f75
Aymanox203 <92544981+Aymanox203@users.noreply.github.com>
2022-11-02 10:27:03 +0100

    added default pk to settings.py

--- commerce/settings.py
+++ commerce/settings.py
@@ -82,6 +82,7 @@ DATABASES = {
 }
 
 AUTH_USER_MODEL = 'auctions.User'
+DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
