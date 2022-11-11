b9e24586c6c825f20943048847f173c6920099e5
Lbacha <aymene_zaz@hotmail.fr>
2022-11-11 07:50:18 +0100

    Wishlist done

--- auctions/util.py
+++ auctions/util.py
@@ -1,8 +1,9 @@
 from locale import currency
 from django.core.exceptions import ValidationError
+from .models import Bid
 
 def validateBid(value,currency):
-    if value>=1:
+    if value<=1:
         return value
     else: raise ValidationError(f"Bid must be higher Than {currency}1")
 
