31e94983303ce77fb522f1317f0fb0d47b9caabf
Lbacha <aymene_zaz@hotmail.fr>
2022-11-17 21:28:18 +0100

    modified url

--- commerce/urls.py
+++ commerce/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-
+from django.conf import settings
 from django.views.static import serve
 from django.conf.urls import url
 
